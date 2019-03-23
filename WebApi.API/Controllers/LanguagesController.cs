893f4297994749ac518cfa3f1faff21aa9546bf9
Mücahit Seba <mucahitseba@gmail.com>
2019-03-23 22:42:47 +0300

    Add project files.

---
+++ WebApi.API/Controllers/LanguagesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.DAL;
+
+namespace WebApi.API.Controllers
+{
+    public class LanguagesController : ApiController
+    {
+        LanguagesDAL languagesDAL=new LanguagesDAL();
+        public IEnumerable<Languages> Get()
+        {
+            return languagesDAL.GetAlLanguageses();
+        }
+
+        public Languages Get(int id)
+        {
+            return languagesDAL.GetLanguageById(id);
+        }
+
+        public Languages Post(Languages language)
+        {
+            return languagesDAL.CreateLanguage(language);
+            
+
+        }
+
+        public Languages Put(int id, Languages language)
+        {
+            return languagesDAL.UpdateLanguage(id, language);
+        }
+
+        public void Delete(int id)
+        {
+            languagesDAL.DeleteLanguage(id);
+        }
+    }
+}
---
+++ WebApi.DAL/LanguagesDAL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.DAL
+{
+    public class LanguagesDAL
+    {
+        ProgrammingDbEntities db=new ProgrammingDbEntities();
+
+        public IEnumerable<Languages> GetAlLanguageses()
+        {
+            return db.Languages;
+        }
+
+        public Languages GetLanguageById(int id)
+        {
+            return db.Languages.Find(id);
+        }
+
+        public Languages CreateLanguage(Languages language)
+        {
+            db.Languages.Add(language);
+            db.SaveChanges();
+            return language;
+        }
+
+        public Languages UpdateLanguage(int id,Languages language)
+        {
+            db.Entry(language).State = EntityState.Modified;
+            db.SaveChanges();
+            return language;
+        }
+
+        public void DeleteLanguage(int id)
+        {
+            db.Languages.Remove(db.Languages.Find(id));
+            db.SaveChanges();
+        }
+    }
+}
